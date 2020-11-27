7ea1d7012d54362ab9332132706ed97a263dc750
Karine66 <shon@live.fr>
2020-11-27 17:07:57 +0100

    toolbar ok

--- app/src/main/java/com/karine/retrieve/ui/mainPage/MainPageActivity.kt
+++ app/src/main/java/com/karine/retrieve/ui/mainPage/MainPageActivity.kt
@@ -1,18 +1,31 @@
 package com.karine.retrieve.ui.mainPage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import androidx.appcompat.app.ActionBar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.karine.retrieve.R
 import com.karine.retrieve.databinding.ActivityMainPageBinding
+
 
 class MainPageActivity : AppCompatActivity() {
 
     private lateinit var mainPageBinding: ActivityMainPageBinding
+    private var ab: ActionBar? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         mainPageBinding = ActivityMainPageBinding.inflate(layoutInflater)
         val view = mainPageBinding.root
         setContentView(view)
+
+        configureToolbar()
     }
+    //for toolbar
+    private fun configureToolbar() {
+        val toolbar: Toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+    }
+
+
 }
